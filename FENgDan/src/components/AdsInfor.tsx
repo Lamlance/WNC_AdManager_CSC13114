b626bffede3b817515c7bfac542d9c5bb073496e
htnphu <phuhanld@gmail.com>
2023-12-02 11:39:03 +0700

    fix: Remove modal, use hover action in AdsInfo

--- FENgDan/src/components/AdsInfor.tsx
+++ FENgDan/src/components/AdsInfor.tsx
@@ -1,62 +1,51 @@
 import React, { useState } from "react";
-import { Modal } from "antd";
 import { EnvironmentOutlined } from "@ant-design/icons";
 import AdsDetail from "./AdsDetail";
 
 function AdsInfor() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
-  const showModal = () => {
-    setIsModalOpen(true);
+  const handleMouseEnter = () => {
+    setIsHovered(true);
   };
 
-  const handleOk = () => {
-    setIsModalOpen(false);
+  const handleMouseLeave = () => {
+    setIsHovered(false);
   };
-
-  const handleCancel = () => {
-    setIsModalOpen(false);
-  };
-
-  const customTitle = (
-    <div className="text-center text-2xl">
-      <p>Chi tiết bảng quảng cáo</p>
-    </div>
-  );
 
   return (
-    <div>
-      <EnvironmentOutlined onClick={showModal} />
-      <Modal
-        title={customTitle}
-        open={isModalOpen}
-        onOk={handleOk}
-        onCancel={handleCancel}
-        width={600}
-      >
-        <p className=" text-xl font-bold">Trụ, cụm Pano</p>
-        <p className="ads_info_location text-xl">
-          42 Võ Thị Sáu, Quận 3, Phường Võ Thị Sáu
-        </p>
-        <p className=" text-xl">
-          <span className="font-bold">Loại bảng: </span> Trụ bảng hiflex
-        </p>
-        <p className=" text-xl">
-          <span className="font-bold">Kích thước: </span> 2.5m x 10m
-        </p>
-        <p className=" text-xl">
-          <span className="font-bold">Hình thức: </span> Quảng cáo thương mại
-        </p>
-        <p className=" text-xl">
-          <span className="font-bold">Phân loại: </span> Nhà ở riêng lẻ
-        </p>
-        <p className=" text-xl">
-          <span className="font-bold">Số lượng: </span> 2 trụ/bảng
-        </p>
-        <p className=" mt-1 text-xl font-bold italic">Đã quy hoạch</p>
+    <div className="ads-info-container">
+      <EnvironmentOutlined onMouseEnter={handleMouseEnter} />
 
-        <AdsDetail />
-      </Modal>
+      {isHovered && (
+        <div className="ads-info-popup" onMouseLeave={handleMouseLeave}>
+          <div className="ads-info-content">
+            <p className="text-xl font-bold">Trụ, cụm Pano</p>
+            <p className="ads_info_location text-xl">
+              42 Võ Thị Sáu, Quận 3, Phường Võ Thị Sáu
+            </p>
+            <p className="text-xl">
+              <span className="font-bold">Loại bảng: </span> Trụ bảng hiflex
+            </p>
+            <p className="text-xl">
+              <span className="font-bold">Kích thước: </span> 2.5m x 10m
+            </p>
+            <p className="text-xl">
+              <span className="font-bold">Hình thức: </span> Quảng cáo thương
+              mại
+            </p>
+            <p className="text-xl">
+              <span className="font-bold">Phân loại: </span> Nhà ở riêng lẻ
+            </p>
+            <p className="text-xl">
+              <span className="font-bold">Số lượng: </span> 2 trụ/bảng
+            </p>
+            <p className="mt-1 text-xl font-bold italic">Đã quy hoạch</p>
+
+            <AdsDetail />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
