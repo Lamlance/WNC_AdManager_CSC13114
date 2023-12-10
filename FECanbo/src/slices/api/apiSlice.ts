0a71f2dcfd9e8187dfead54c731c60608f1d653c
An Tran Van <86640630+antrvan746@users.noreply.github.com>
2023-12-10 23:18:42 +0700

    chore: add style and add redux toolkit

---
+++ FECanbo/src/slices/api/apiSlice.ts
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+export const apiSlice = createApi({
+    reducerPath: "api",
+    baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
+    endpoints: builder => ({
+        getAllAdsInfo: builder.query({
+            query: () => "/quang-cao"
+        }),
+        getAnAdsInfo: builder.query({
+            query: (id) => `/quang-cao/${id}`
+        }),
+        getAllAdsRequest: builder.query({
+            query: () => "/don-dang-ky-quang-cao"
+        }),
+        getAnAdsRequest: builder.query({
+            query: (id) => `/don-dang-ky-quang-cao/${id}`
+        }),
+        getAllReportInfo: builder.query({
+            query: () => "/bao-cao-nguoi-dan/"
+        }),
+        getAReportInfo: builder.query({
+            query: (id) => `/bao-cao-nguoi-dan/${id}` 
+        })
+    })
+})
--- FECanbo/src/types.ts
+++ FECanbo/src/types.ts
@@ -16,6 +16,7 @@ export interface AdRequest {
 }
 
 export interface AdsInfoRecord {
+  id: string;
   adsType: string;
   address: string;
   generalInfo: {
@@ -28,15 +29,19 @@ export interface AdsInfoRecord {
   contentType: string;
   placeType: string;
   status: string;
+  effectDate: string;
+  expireDate: string;
 }
 
 export interface ReportInfoRecord {
+  id: string;
   reporterInfo: {
     name: string;
     phone: string;
   };
   adsAddress: string;
   reportType: string;
+  reportContent: string;
   reportTime: Date;
   status: string;
 }
