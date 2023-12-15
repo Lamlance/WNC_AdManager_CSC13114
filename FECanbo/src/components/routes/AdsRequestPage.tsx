ea457ade03f5ed6f4a43e86946ef3283423ce913
unknown <phanthihuunien@gmail.com>
2023-12-15 08:07:36 +0700

    add style for detailed request, change type

--- FECanbo/src/components/routes/AdsRequestPage.tsx
+++ FECanbo/src/components/routes/AdsRequestPage.tsx
@@ -1,63 +1,65 @@
 import { useEffect, useState } from "react";
 import { AdRequest } from "../../types";
 import AdsRequest from "../ads-request/AdsRequest";
-
+import AdsRequestForm from "../ads-request/AdsRequestForm";
+import { Button } from "antd";
 const data: AdRequest[] = [
   {
     requestId: "1",
     panoContent: "Quảng cáo",
-    panoTitle: "Quảng cáo trà sữa Phúc Long",
     position: "42 Võ Thị Sáu, Quận 3, Phường Võ Thị Sáu",
-    bookingAgency: "TNVH HN",
+    companyName: "TNVH HN",
     email: "pthn@gmail.com",
     phoneNumber: "19001001",
     address: "227 NCV, Q5, HCM",
-    panoDetailedContent:
-      "Trà sữa Phúc Long không đặc biệt và hơi mắc nhưng uống cũng được",
-    rentalPeriod: "12/11/2022 - 12/11/2024",
+    effectedDate: "12/11/2022 ",
+    expiredDate: "22/12/2023",
     status: "Đang xử lý",
     image: "",
-    tags: ["nice", "developer"],
   },
   {
     requestId: "2",
     panoContent: "Quảng cáo",
-    panoTitle: "Quảng cáo trà sữa Phúc Long",
     position: "42 Võ Thị Sáu, Quận 3, Phường Võ Thị Sáu",
-    bookingAgency: "TNVH HN",
+    companyName: "TNVH HN",
     email: "pthn@gmail.com",
     phoneNumber: "19001001",
     address: "227 NCV, Q5, HCM",
-    panoDetailedContent:
-      "Trà sữa Phúc Long không đặc biệt và hơi mắc nhưng uống cũng được",
-    rentalPeriod: "12/11/2022 - 12/11/2024",
+    effectedDate: "12/11/2022 ",
+    expiredDate: "22/12/2023",
     status: "Đã phê duyệt",
     image: "",
-    tags: ["nice", "developer"],
   },
   {
     requestId: "3",
     panoContent: "Quảng cáo",
-    panoTitle: "Quảng cáo trà sữa Phúc Long",
     position: "42 Võ Thị Sáu, Quận 3, Phường Võ Thị Sáu",
-    bookingAgency: "TNVH HN",
+    companyName: "TNVH HN",
     email: "pthn@gmail.com",
     phoneNumber: "19001001",
     address: "227 NCV, Q5, HCM",
-    panoDetailedContent:
-      "Trà sữa Phúc Long không đặc biệt và hơi mắc nhưng uống cũng được",
-    rentalPeriod: "12/11/2022 - 12/11/2024",
+    effectedDate: "12/11/2022 ",
+    expiredDate: "22/12/2023",
     status: "Đã hủy",
     image: "",
-    tags: ["nice", "developer"],
   },
 ];
 
 function AdsRequestPage() {
   const [selectedAds, setSelectedAds] = useState<AdRequest | null>(null);
+  const [isFormVisible, setIsFormVisible] = useState(false);
 
   const showDetails = (record: AdRequest) => {
     setSelectedAds(record);
+  };
+
+  const showForm = () => {
+    console.log("Button clicked - showForm");
+    setIsFormVisible(true);
+  };
+
+  const closeForm = () => {
+    setIsFormVisible(false);
   };
 
   useEffect(() => {
@@ -67,7 +69,25 @@ function AdsRequestPage() {
   }, []);
 
   return (
-    <AdsRequest data={data} onRowClick={showDetails} selectedAd={selectedAds} />
+    <>
+      <Button
+        onClick={showForm}
+        style={{
+          marginBottom: 16,
+          backgroundColor: "#1890ff",
+          color: "#fff",
+        }}
+      >
+        Thêm yêu cầu
+      </Button>
+      <AdsRequest
+        data={data}
+        onRowClick={showDetails}
+        selectedAd={selectedAds}
+      />
+
+      <AdsRequestForm isVisible={isFormVisible} onCancel={closeForm} />
+    </>
   );
 }
 export default AdsRequestPage;
