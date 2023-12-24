06c675d4443b4a565d55b9ea9e38e5b5cccc2343
An Tran Van <86640630+antrvan746@users.noreply.github.com>
2023-12-24 09:01:26 +0700

    fix: fix auth endpoints

--- BECanbo/src/routes/auth.ts
+++ BECanbo/src/routes/auth.ts
@@ -1,17 +1,24 @@
 import { Router } from "express";
 import { CallAndCatchAsync } from "../utils/CallCatch";
-import { getAnUserByUsername, createAnUser, getUserById } from "../db/service/user";
+import {
+  getAnUserByUsername,
+  createAnUser,
+  getUserById,
+} from "../db/service/user";
 import { UserDto } from "../types/dto/user";
 import jwt from "jsonwebtoken";
 import passport from "passport";
 import bcrypt from "bcrypt";
+import { configDotenv } from "dotenv";
+
+configDotenv();
 
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || "";
-const SALT_ROUNDS = process.env.SALT_ROUNDS || 0;
+const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS || "0");
 
 const router = Router();
 
-router.post("/login", async (req, res, next) => {
+router.get("/login", async (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
 
@@ -21,28 +28,35 @@ router.post("/login", async (req, res, next) => {
     return res.status(500).json({ err: result.error });
   } else {
     const user = result.data[0];
-    const passwordMatch = await bcrypt.compare(password, user.mat_khau);
 
-    if (passwordMatch) {
-      const authToken = jwt.sign(user, JWT_SECRET_KEY);
-      return res.status(200).json({ token: authToken });
+    if (user) {
+      const passwordMatch = await bcrypt.compare(password, user.mat_khau);
+
+      if (passwordMatch) {
+        const authToken = jwt.sign(user, JWT_SECRET_KEY);
+        return res.status(200).json({ token: authToken });
+      } else {
+        return res.status(400).json({ msg: "Invalid authentication token " });
+      }
     } else {
-      return res.status(400).json({ msg: "Invalid authentication token "});
+      return res
+        .status(400)
+        .json({ msg: "Invalid authentication information" });
     }
-  } 
+  }
 });
 
 router.post("/register", async (req, res, next) => {
-  const saltRounds = 10;
+  const saltRounds = SALT_ROUNDS;
   const salt = await bcrypt.genSalt(saltRounds);
 
   const user: UserDto = {
     username: req.body.username,
     password: await bcrypt.hash(req.body.password, salt),
-    role: req.body.role
+    role: req.body.role,
   };
-  const result = await CallAndCatchAsync(createAnUser, user);
 
+  const result = await CallAndCatchAsync(createAnUser, user);
   if (!result.success) {
     const error = result.error;
     return res.status(500).json({ err: error });
